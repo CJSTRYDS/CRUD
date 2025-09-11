ca050310e49557e1d3bd77273ee3187246a2a9e8
CJSTRYDS <corey@coreystrydom.com>
2025-09-11 09:06:40 +0100

    slides 3 finish

--- website/views.py
+++ website/views.py
@@ -1,8 +1,112 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm, LoginForm, CreateRecordForm, UpdateRecordForm
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
+from .models import Record
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request, 'pages/index.html')
 
 def login(request):
-    return render(request, 'pages/login.html')
+    form = LoginForm()
+
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+                return redirect('dashboard')
+
+    context = {'form':form}
+    return render(request, 'pages/login.html', context=context)
+
+
+def home (request):
+    return render (request, 'pages/index.html')
+
+# register a user
+def register (request) :
+    form = CreateUserForm ()
+
+    if request.method == "POST" :
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Account created successfully!")
+            return redirect('login')
+
+    context = {'form': form}
+
+    return render(request, 'pages/register.html', context=context)
+
+def logout(request):
+
+    auth.logout(request)
+    messages.success(request,"Log out successfull!")
+    return redirect("login")
+
+
+@login_required(login_url='login')
+def dashboard(request):
+
+    my_records = Record.objects.all()
+    context = {'records': my_records}
+
+    return render (request, 'pages/dashboard.html',context=context)
+
+@login_required(login_url='login')
+def create_record(request):
+
+    form = CreateRecordForm()
+    if request.method == "POST":
+        form = CreateRecordForm(request.POST)
+
+        if form.is_valid():
+            form. save ()
+            messages.success(request,"record created successfully!")
+            return redirect ("dashboard")
+        
+    context = {'create_form': form}
+    return render (request, 'pages/create_record.html',context=context)
+
+@login_required(login_url='login')
+def singular_record(request, pk):
+
+        one_record = Record.objects.get(id=pk)
+        context = {'record': one_record}
+        return render (request, 'pages/view_record.html', context = context)
+
+@login_required(login_url='login')
+def update_record(request, pk):
+    new_record = Record.objects.get(pk=pk)
+
+    form = UpdateRecordForm()
+    if request.method == "POST":
+        form = UpdateRecordForm(request.POST, instance = new_record)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request,"record updated successfully!")
+            return redirect ("dashboard")
+        
+    context = {'update_form': form}
+    return render (request, 'pages/update_record.html',context=context)
+
+
+@login_required(login_url='login')
+def delete_record(request, pk):
+    record = Record.objects.get(id=pk)
+    record.delete()
+    messages.success(request,"record deleted successfully!")
+
+    return redirect("dashboard")
+
